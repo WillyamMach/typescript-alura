a12c26e9432e0af5ca07651ae78d4bfb94c77d94
Willyam Mach <willyam.mach@gmail.com>
2024-06-20 13:35:48 -0300

    baixar arquivo alura

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -5,10 +5,10 @@ export abstract class View<T> {
 
     constructor(seletor: string, escapar?: boolean) {
         const elemento = document.querySelector(seletor);
-        if(elemento) {
+        if (elemento) {
             this.elemento = elemento as HTMLElement;
         } else {
-            throw Error(`Seletor ${seletor} não existe no DOM.`)
+            throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
         }
         if (escapar) {
             this.escapar = escapar;
@@ -17,11 +17,11 @@ export abstract class View<T> {
 
     public update(model: T): void {
         let template = this.template(model);
-        if(this.escapar) {
-            template = template.replace(/<script>[\s\S]*?<\/script>/, "")
+        if (this.escapar) {
+            template = template
+                .replace(/<script>[\s\S]*?<\/script>/, '');
         }
         this.elemento.innerHTML = template;
-        const t2 = performance.now();
     }
 
     protected abstract template(model: T): string;
