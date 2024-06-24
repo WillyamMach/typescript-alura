6093c98079b41e5f6f289321eefb0c60033fa70c
Willyam Mach <willyam.mach@gmail.com>
2024-06-24 14:07:59 -0300

    deu por hj

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -5,7 +5,6 @@ import { View } from './view.js';
 
 export class NegociacoesView extends View<Negociacoes> {
 
-    @escapar
     protected template(model: Negociacoes): string {
         return `
         <table class="table table-hover table-bordered">
