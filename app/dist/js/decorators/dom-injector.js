5bc7ce1e9f1bb72559a7093e56a3fd482257502b
Willyam Mach <willyam.mach@gmail.com>
2024-06-25 13:54:25 -0300

    feat: melhorando domInjector

---
+++ app/dist/js/decorators/dom-injector.js
@@ -0,0 +1,13 @@
+export function domInjector(seletor) {
+    let elemento;
+    return function (target, propertyKey) {
+        const getter = function () {
+            if (!elemento) {
+                elemento = document.querySelector(seletor);
+                console.log(`buscando elemento do dom com o seletor ${seletor} encontrado em ${propertyKey}`);
+            }
+            return elemento;
+        };
+        Object.defineProperty(target, propertyKey, { get: getter });
+    };
+}
--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,8 +1,18 @@
 export function domInjector(seletor: string) {
+    let elemento: HTMLElement;
     return function(target: any, propertyKey: string) {
-         const getter = function() {
-            const elemento = document.querySelector(seletor);
+        const getter = function() {
+            if(!elemento) {
+                elemento = <HTMLElement>document.querySelector(seletor);
+                console.log(`buscando elemento do dom com o seletor ${seletor} encontrado em ${propertyKey}`)
+            }
+
             return elemento;
-         }
+        }
+        Object.defineProperty(
+            target, 
+            propertyKey, 
+            {get: getter}
+        ); 
     }
 }
