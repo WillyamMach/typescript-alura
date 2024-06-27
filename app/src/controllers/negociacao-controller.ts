3b0dbe7880b3e233306e3980e8ba0eadb0082d7f
Willyam Mach <willyam.mach@gmail.com>
2024-06-27 13:56:38 -0300

    limitando console.logs

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -6,6 +6,7 @@ import { NegociacoesDoDia } from '../interfaces/negociacao-do-dia.js';
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
 import { NegociacoesService } from '../services/negociacoes-service.js';
+import { imprimir } from '../utils/imprimir.js';
 import { MensagemView } from '../views/mensagem-view.js';
 import { NegociacoesView } from '../views/negociacoes-view.js';
 
@@ -44,8 +45,7 @@ export class NegociacaoController {
         }
 
         this.negociacoes.adiciona(negociacao);
-        console.log(negociacao.paraTexto())
-        console.log(this.negociacoes.paraTexto())
+        imprimir(negociacao);
         this.limparFormulario();
         this.atualizaView();
     }
